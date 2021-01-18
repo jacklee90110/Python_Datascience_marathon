c7a1cb30265f89cee86a5895b84eca6bfad365d1
Guang-Jie Lee <jacklee90110@gmail.com>
2021-01-18 20:43:10 +0800

    initial commit

---
+++ Day-1/numpy_basic.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# print (np.__version__)		# check numpy version
+# define print as p
+
+# ========== Day-1 numpy basics ==========
+
+# ***** part-1 numpy array
+
+a = np.array ([1,2,3,4,5])
+print (a)
+b = np.array ([6,7,8,9,10])
+
+print (type (a) )  
+print (type (b) )
